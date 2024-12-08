a1dea71d2cbe5ee53cc304bd617745ea075d960e
joaquin-sanchez-tonani <joaquinsancheztonani15@gmail.com>
2024-12-08 08:33:42 -0300

    Fix image of public

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -4,23 +4,23 @@ import './technologies.css';
 
 export default function Technologies() {
     const masteredList = [
-        { img: "public/python.png", name: "Python", time: "2 years", text: "Variables, functions, loops, conditionals, lists, dictionaries, object-oriented programming (OOP), file handling, APIs, modules, libraries.", url: "https://www.python.org" },
-        { img: "public/react.png", name: "React", time: "2 years", text: "Components, JSX, props, state, hooks, useState, event handling, routing, context API, functional components.", url: "https://reactjs.org" },
-        { img: "public/js.png", name: "Javascript", time: "2 years", text: "Variables, functions, loops, arrays, objects, DOM manipulation, events, APIs, async/await, promises.", url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
-        { img: "public/html.png", name: "HTML", time: "2 years", text: "Tables, forms, lists, links, images, multimedia elements, semantic structure, forms with validation, accessibility.", url: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+        { img: "/python.png", name: "Python", time: "2 years", text: "Variables, functions, loops, conditionals, lists, dictionaries, object-oriented programming (OOP), file handling, APIs, modules, libraries.", url: "https://www.python.org" },
+        { img: "/react.png", name: "React", time: "2 years", text: "Components, JSX, props, state, hooks, useState, event handling, routing, context API, functional components.", url: "https://reactjs.org" },
+        { img: "/js.png", name: "Javascript", time: "2 years", text: "Variables, functions, loops, arrays, objects, DOM manipulation, events, APIs, async/await, promises.", url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+        { img: "/html.png", name: "HTML", time: "2 years", text: "Tables, forms, lists, links, images, multimedia elements, semantic structure, forms with validation, accessibility.", url: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
     ];
     
     const learningList = [
-        { img: "public/css.png", name: "CSS", time: "2 years", text: "Selectors, properties, animations, transitions, responsiveness, media queries, pseudo-classes", url: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
-        { img: "public/mysql.png", name: "MySQL", time: "Less than 1 year.", text: "Databases, tables, queries, joins, indexes, CRUD operations.", url: "https://www.mysql.com" },
-        { img: "public/node.png", name: "Node", time: "Less than 1 year.", text: "Modules, Express, routing, APIs, asynchronous programming.", url: "https://nodejs.org" },
-        { img: "public/astro.png", name: "Astro", time: "Less than 1 year.", text: "Components, JSX, rendering, static site generation, integrations, markdown, server-side rendering, partial hydration.", url: "https://astro.build" },
-        { img: "public/c.png", name: "C", time: "Less than 1 year.", text: "Variables, functions, loops, conditionals, pointers, memory management, structs, arrays.", url: "https://en.cppreference.com/w/c" },
+        { img: "/css.png", name: "CSS", time: "2 years", text: "Selectors, properties, animations, transitions, responsiveness, media queries, pseudo-classes", url: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+        { img: "/mysql.png", name: "MySQL", time: "Less than 1 year.", text: "Databases, tables, queries, joins, indexes, CRUD operations.", url: "https://www.mysql.com" },
+        { img: "/node.png", name: "Node", time: "Less than 1 year.", text: "Modules, Express, routing, APIs, asynchronous programming.", url: "https://nodejs.org" },
+        { img: "/astro.png", name: "Astro", time: "Less than 1 year.", text: "Components, JSX, rendering, static site generation, integrations, markdown, server-side rendering, partial hydration.", url: "https://astro.build" },
+        { img: "/c.png", name: "C", time: "Less than 1 year.", text: "Variables, functions, loops, conditionals, pointers, memory management, structs, arrays.", url: "https://en.cppreference.com/w/c" },
     ];
     
     const tools = [
-        { img: "public/github.png", name: "Git", time: "Less than 1 year.", text: "Version control, branches, commits, merges, rebases, remote repositories, conflict resolution.", url: "https://git-scm.com" },
-        { img: "public/git.png", name: "GitHub", time: "Less than 1 year.", text: "Repositories, cloning, pull requests, branches, forks, merging, issues, actions, collaboration.", url: "https://github.com" },
+        { img: "/github.png", name: "Git", time: "Less than 1 year.", text: "Version control, branches, commits, merges, rebases, remote repositories, conflict resolution.", url: "https://git-scm.com" },
+        { img: "/git.png", name: "GitHub", time: "Less than 1 year.", text: "Repositories, cloning, pull requests, branches, forks, merging, issues, actions, collaboration.", url: "https://github.com" },
     ];
     
 
